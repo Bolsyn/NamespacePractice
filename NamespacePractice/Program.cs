63792d55fa360b58a994a5dfff00d3a7de009816
Bolsyn <Serikagis@yandex.ru>
2020-12-14 22:52:01 +0600

    Change Token and phone to user input.

--- NamespacePractice/Program.cs
+++ NamespacePractice/Program.cs
@@ -12,25 +12,24 @@ class Program
         int signUp = Console.Read()-48;
 
         var rand = new Random();
-        int code = rand.Next() % 89999 + 10000;        
-      
+        int code = rand.Next() % 89999 + 10000;       
         if (signUp == 1)
         {
             Console.WriteLine("Write phone number, (+XYYYZZZZZZZ)");
             Console.Write("where, X - code countru, Y - code operator, Z - phone number: ");
-            // string phone = Console.ReadLine();
+            Console.ReadLine();
+            string phone = Console.ReadLine();
 
             string accountSid = "AC297c271ef56c987b77ec37e2d3da4206";
-            string authToken = "fa7a0ae91568ff6cbcbedf7004a94f8d";
+            string authToken = "9ae12edd18e44255eacd5c46bd5d5bf1";
 
             TwilioClient.Init(accountSid, authToken);           
 
             var message = MessageResource.Create(
                 body: $"Your code is {code}",
                 from: new Twilio.Types.PhoneNumber("+12059472980"),
-                to: new Twilio.Types.PhoneNumber("+77012081260")
+                to: new Twilio.Types.PhoneNumber(phone)
             );
-
 
             Console.Write("Please write code on your cellphone: ");
             string inputCode = Console.ReadLine();
